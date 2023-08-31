2e4c5d7cd7698af1bd3722f0f5272f000e119c64
Andreas Bigger <abigger87@gmail.com>
2023-08-31 10:40:13 -0400

    Port monitoring to resubscriptions.

--- op-challenger/game/service.go
+++ op-challenger/game/service.go
@@ -76,7 +76,7 @@ func NewService(ctx context.Context, logger log.Logger, cfg *config.Config) (*Se
 			return fault.NewGamePlayer(ctx, logger, m, cfg, dir, addr, txMgr, client)
 		})
 
-	monitor := newGameMonitor(logger, cl, loader, sched, cfg.GameWindow, client.BlockNumber, cfg.GameAllowlist)
+	monitor := newGameMonitor(logger, cl, loader, sched, cfg.GameWindow, client.BlockNumber, cfg.GameAllowlist, client)
 
 	m.RecordInfo(version.SimpleWithMeta)
 	m.RecordUp()
